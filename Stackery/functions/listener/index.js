444b12d1e993f36b87af134db7d96604d9c79f81
Toby Fee <TobyFee@gmail.com>
2018-05-10 14:40:37 -0700

    return comms

--- Stackery/functions/listener/index.js
+++ Stackery/functions/listener/index.js
@@ -1,5 +1,6 @@
 module.exports = async message => {
   console.dir(message);
-
-  return {};
+  return {
+    greeting: "hello"
+  };
 }
--- Stackery/functions/sender/index.js
+++ Stackery/functions/sender/index.js
@@ -6,18 +6,24 @@ var lambda = new aws.Lambda({
 module.exports = async event => {
   const ports = JSON.parse(process.env.STACKERY_PORTS);
   const functionName = ports[0][0].functionName
-  console.dir(ports);
-  console.log(functionName)
+    // console.dir(ports);
+    // console.log(functionName)
 
   lambda.invoke({
     FunctionName: functionName,
     Payload: JSON.stringify(event, null, 2) // pass params
   }, function(error, data) {
+    console.log('raw data')
+    console.dir(data)
     if (error) {
+      console.log('lack of success')
       return error
     }
     if (data.Payload) {
+      console.log('success!')
       return data.Payload
+    } else {
+      return {}
     }
   })
 
