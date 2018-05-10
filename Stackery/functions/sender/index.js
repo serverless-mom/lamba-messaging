984b1a042431db96e434c90efc3b2ca2a8a7d58c
Toby Fee <TobyFee@gmail.com>
2018-05-10 14:28:29 -0700

    actual message sending! possibly

--- Stackery/functions/sender/index.js
+++ Stackery/functions/sender/index.js
@@ -1,8 +1,24 @@
-module.exports = async message => {
+var aws = require('aws-sdk');
+var lambda = new aws.Lambda({
+  region: 'us-west-2' //change to your region
+});
+
+module.exports = async event => {
   const ports = JSON.parse(process.env.STACKERY_PORTS);
   const functionName = ports[0][0].functionName
   console.dir(ports);
   console.log(functionName)
 
-  return {};
+  lambda.invoke({
+    FunctionName: functionName,
+    Payload: JSON.stringify(event, null, 2) // pass params
+  }, function(error, data) {
+    if (error) {
+      return error
+    }
+    if (data.Payload) {
+      return data.Payload
+    }
+  })
+
 }
